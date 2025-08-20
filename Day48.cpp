eb92137e4d7d52638d0fc04a976c887f56b4154b
aayushbhati <124025774+aayushbhati@users.noreply.github.com>
2025-08-20 22:21:18 +0530

    Update Day48.cpp

--- Day48.cpp
+++ Day48.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int maxi=INT_MIN;
-        int ind=0;
-        for(int i=1;i<nums.size();i++){
-            if(i+1<nums.size()&&nums[i-1]<nums[i] && nums[i+1]<nums[i])return i;
+        int start = 0, end = nums.size()-1;
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] > nums[mid+1])
+                end = mid;
+            else
+                start = mid + 1;
         }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>maxi){
-            maxi=max(maxi,nums[i]);
-            ind=i;
-            }
-        }
-        return ind;
+        return start;
     }
 };
